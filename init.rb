929b7d83274bbeca1a4d00cabefa2ce054fc6979
lexa62 <lexa62@tut.by>
2018-08-16 17:27:22 +0300

    Init

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_issue_progress do
+  name 'Redmine Issue Progress plugin'
+  author '//twinslash'
+  description 'This is a plugin for Redmine'
+  version '1.0.0'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
