15f901a6a52d776d77117686518787d93cafcf71
test-for-test <42804057+test-for-test@users.noreply.github.com>
2018-09-28 17:59:17 +0400

    Update sidekiq_initialization.rb

--- lib/sidekiq_initialization.rb
+++ lib/sidekiq_initialization.rb
@@ -1,8 +1,8 @@
 # Copyright (C) 2018 Tecforce - All rights reserved.
 
-# This file is part of “Done ration via time plugin”.
+# This file is part of “Done ratio via time plugin”.
 
-# “Done ration via time plugin” or its sources can not be copied and/or
+# “Done ratio via time plugin” or its sources can not be copied and/or
 # distributed without the express permission of Tecforce. Unauthorized copying
 # of this file, via any media, is strictly prohibited.
 
