d4cf6bd3f01b44398e1152f8753bb7dfa22a41a2
lexa62 <lexa62@tut.by>
2018-09-28 13:21:55 +0300

    Increase sidekiq status expiration time

--- lib/sidekiq_initialization.rb
+++ lib/sidekiq_initialization.rb
@@ -14,15 +14,15 @@ require 'sidekiq-status'
 
 Sidekiq.configure_client do |config|
   # accepts :expiration (optional)
-  Sidekiq::Status.configure_client_middleware config, expiration: 5.minutes
+  Sidekiq::Status.configure_client_middleware config, expiration: 2.hours
   config.redis = { url: 'redis://localhost:6379' }
 end
 
 Sidekiq.configure_server do |config|
   # accepts :expiration (optional)
-  Sidekiq::Status.configure_server_middleware config, expiration: 5.minutes
+  Sidekiq::Status.configure_server_middleware config, expiration: 2.hours
 
   # accepts :expiration (optional)
-  Sidekiq::Status.configure_client_middleware config, expiration: 5.minutes
+  Sidekiq::Status.configure_client_middleware config, expiration: 2.hours
   config.redis = { url: 'redis://localhost:6379' }
 end
