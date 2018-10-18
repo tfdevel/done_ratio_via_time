cb62c4052de6a26c888cdb153583e1b934d652c4
krokhin <krokhin@tecforce.ru>
2018-10-18 14:08:51 +0400

    Updated Readme.md

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
-# Copyright (C) 2018 Tecforce - All rights reserved.
-
-# This file is part of “Done ratio via time plugin”.
-
-# “Done ratio via time plugin” or its sources can not be copied and/or
-# distributed without the express permission of Tecforce. Unauthorized copying
-# of this file, via any media, is strictly prohibited.
-
-# Proprietary and confidential.
-# For more details please contact sales@tecforce.ru.
+# Licensed under GNU GPL 2.0
+# Author: Tecforce
+# Website: http://tecforce.ru
 
 get 'done_ratio_via_time_settings/edit', to: 'done_ratio_via_time_settings#edit'
 post 'done_ratio_via_time_settings/update', to: 'done_ratio_via_time_settings#update'
--- db/migrate/20180828095325_add_default_done_ratio_calculation_type_to_projects.rb
+++ db/migrate/20180828095325_add_default_done_ratio_calculation_type_to_projects.rb
@@ -1,13 +1,6 @@
-# Copyright (C) 2018 Tecforce - All rights reserved.
-
-# This file is part of “Done ratio via time plugin”.
-
-# “Done ratio via time plugin” or its sources can not be copied and/or
-# distributed without the express permission of Tecforce. Unauthorized copying
-# of this file, via any media, is strictly prohibited.
-
-# Proprietary and confidential.
-# For more details please contact sales@tecforce.ru.
+# Licensed under GNU GPL 2.0
+# Author: Tecforce
+# Website: http://tecforce.ru
 
 class AddDefaultDoneRatioCalculationTypeToProjects < ActiveRecord::Migration
   def change
