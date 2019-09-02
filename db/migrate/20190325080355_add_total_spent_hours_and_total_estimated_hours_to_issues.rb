d764efee34901202df2445775439f868ad41cc81
test-for-test <42804057+test-for-test@users.noreply.github.com>
2019-09-02 16:51:08 +0400

    Update 20190325080355_add_total_spent_hours_and_total_estimated_hours_to_issues.rb

--- db/migrate/20190325080355_add_total_spent_hours_and_total_estimated_hours_to_issues.rb
+++ db/migrate/20190325080355_add_total_spent_hours_and_total_estimated_hours_to_issues.rb
@@ -1,3 +1,7 @@
+# Licensed under GNU GPL 2.0
+# Author: Tecforce
+# Website: http://tecforce.ru
+
 class AddTotalSpentHoursAndTotalEstimatedHoursToIssues < ActiveRecord::Migration
   def change
     add_column :issues, :total_estimated_hours, :float, default: nil
